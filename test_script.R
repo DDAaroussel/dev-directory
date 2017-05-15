19d882c57c023c470a6f6c667b24ded27c569a3f
arousselDDA <alex@deepdiveanalytics.com.au>
2017-05-15 22:23:32 +1000

    A comment explaining that we now need to add nycflights package was added.

--- test_script.R
+++ test_script.R
@@ -1,3 +1,7 @@
 #This is a test script to verify that DDA's BitBucket repository is working 
 
 library(ggplot)
+
+#Now that we have loaded the ggplot library, we can load the nycflights library
+
+library(nycflights13)
