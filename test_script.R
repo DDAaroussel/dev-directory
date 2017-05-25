69c43f750e1b3def63004850afc93fff57f333da
arousselDDA <alex@deepdiveanalytics.com.au>
2017-05-25 23:22:11 +1000

    Added a comment describing the printing of A

--- test_script.R
+++ test_script.R
@@ -1,9 +1,6 @@
-#This is a test script to verify that DDA's BitBucket repository is working 
 
-library(ggplot)
+#Here I am creating a variable called A   
+a <- 2+2 
 
-#Now that we have loaded the ggplot library, we can load the nycflights library
-
-library(nycflights13)
-
-a <- 1+1 
+#here I am printing A
+print(a)
