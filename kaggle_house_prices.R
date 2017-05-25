dfa9c1ecd4ff2eb33e7d09654d0dffa988d42e13
arousselDDA <alex@deepdiveanalytics.com.au>
2017-05-25 23:21:46 +1000

    This the initial commit for my Kaggle house prices tutorial script

---
+++ kaggle_house_prices.R
@@ -0,0 +1,23 @@
+#This a tutorial script to accompany the Kaggle housing market analysis
+
+
+# Load libraries ----------------------------------------------------------
+
+install.packages("data.table")
+install.packages("testthat")
+install.packages("gridExtra")
+install.packages("corrplot")
+install.packages("GGally")
+install.packages("e1071")
+library(dplyr)
+library(ggplot2)
+library(data.table)
+library(testthat)
+library(gridExtra)
+library(GGally)
+library(e1071)
+
+
+# Load data ---------------------------------------------------------------
+
+
